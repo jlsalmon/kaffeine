a265692df1847a0f38419e811383c35ac81ffba0
Justin Lewis Salmon <j.lewis@inbox.com>
2012-03-09 10:31:42 +0000

    improved error handling

--- Release/src/server/kaffeine.d
+++
@@ -1,6 +0,0 @@
-src/server/kaffeine.d src/server/kaffeine.o: ../src/server/kaffeine.c \
- ../src/server/kaffeine.h ../src/server/vcp.h
-
-../src/server/kaffeine.h:
-
-../src/server/vcp.h:
--- Release/src/server/vcp.d
+++
@@ -1,4 +0,0 @@
-src/server/vcp.d src/server/vcp.o: ../src/server/vcp.c \
- ../src/server/vcp.h
-
-../src/server/vcp.h:
